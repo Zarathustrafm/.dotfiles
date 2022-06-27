071ca57f83556894cdb0b34198580edf65943e82
Zarathustra <73062084+Zarathustrafm@users.noreply.github.com>
2022-06-27 19:27:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-# Install my fundamental packages
+cd ~/.dotfiles/
+
+echo "\n Installing fundamental packages: \n"
+sleep 2
 sh ./install.sh
 
-# Make zsh default
+echo "\n Making ZSH default: \n"
+sleep 2
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
 touch ~/.zshenv ~/.zprofile ~/.zshrc ~/.zlogin
 
-# Stow dot files
+echo "\n Stow dot files: \n"
+sleep 2
+cd ~/.dotfiles/
 stow alacritty 
 stow easyeffects 
 stow nvim 
@@ -16,9 +22,7 @@ stow starship
 stow vim 
 stow zsh
 
-# Install vim-plug
+echo "\n Intalling vim-plug: \n"
+sleep 2
 sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Install nvim plugins
-nvim --headless +PlugInstall +qall
